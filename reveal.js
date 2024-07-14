27fd8ed9d4102b0d444ea787ab9ecf6aa44d47f0
Louis-Eiden <eidenlouis.mt@gmail.com>
2024-07-14 20:36:19 +0200

    fix animations

--- reveal.js
+++ reveal.js
@@ -1,3 +1,20 @@
   window.addEventListener('load', function() {
     document.getElementById('header').style.transform = 'scaleY(1)';
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const subscribeSection = document.querySelector('#subscribe #subscribe-content');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          subscribeSection.classList.add('reveal');
+          observer.unobserve(subscribeSection);
+        }
+      });
+    }, { threshold: 0.1 });
+
+    observer.observe(subscribeSection);
+  });
+
+  
